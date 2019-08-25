64bfb781c9fbbc939a431c3117cc5bc75aa6371c
liuxiaolin <478528180@qq.com>
2019-08-25 23:33:22 +0800

    feat: 手动实现instanceof

---
+++ javascript/instanceof.js
@@ -0,0 +1,10 @@
+/**
+ * a instanceof Object
+ * 判断Object.prototype是否在a的原型链上
+ */
+function instanceOf(target, origin) {
+  const proto = target.__proto__;
+  if(!proto) return false;
+  if(proto === origin.prototype) return true;
+  return instanceOf(proto, origin);
+}
