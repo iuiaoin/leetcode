b7ab8d4ec594779ac0368a1ce43b47cbab78923d
liuxiaolin <478528180@qq.com>
2019-08-27 19:52:51 +0800

    feat: [958] Check Completeness of a Binary Tree

--- 958.check-completeness-of-a-binary-tree.js
+++ 958.check-completeness-of-a-binary-tree.js
@@ -15,29 +15,20 @@
  * @return {boolean}
  */
 /**
- * 层序遍历，当遇到有的节点时，如果出现过null返回false，如果是最后一层返回true，否则如果出现了null返回false
+ * 层序遍历，标记节点的位置pos，遍历完之后比较pos和节点总数是否相等
  */
-var isCompleteTree = function(root) {
-  if(!root) return true;
-  let queue = [ root ];
-  while(queue.length) {
-    let len = queue.length,
-        hasNull = false;
-    for(let i = 0; i < len; i++) {
-      let node = queue.shift();
-      if(node) {
-        if(hasNull) return false;
-        queue.push(node.left);
-        queue.push(node.right);
-      } else {
-        hasNull = true;
-      }
-    }
-    if(queue.every(v => v === null)) {
-      return true;
-    } else if(hasNull) {
-      return false;
-    }
+var isCompleteTree = function (root) {
+  if (!root) return true;
+  let queue = [[root, 1]],
+      count = 0,
+      pos;
+  while (queue.length) {
+    count++;
+    let arr = queue.shift(),
+        node = arr[0];
+    pos = arr[1];
+    if(node.left) queue.push([node.left, pos * 2]);
+    if(node.right) queue.push([node.right, pos * 2 + 1]);
   }
+  return pos === count;
 };
-
