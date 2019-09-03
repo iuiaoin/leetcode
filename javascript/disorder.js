d15385eff55b66616d71678ae39140bd4ffecc31
liuxiaolin <478528180@qq.com>
2019-09-03 10:05:27 +0800

    feat: 数组乱序-洗牌算法

--- javascript/disorder.js
+++ javascript/disorder.js
@@ -1,16 +1,16 @@
 // 数组乱序-洗牌算法
 
 /**
- * 从最后一个元素开始，随机选择一个元素交换，直至第一个
+ * 从第一个元素开始，随机选择一个元素交换，直至第一个
  * @param {*} arr 
  */
 function disorder(arr) {
   let len = arr.length,
-      cur = len - 1;
-  while(cur > -1) {
+      cur = 0;
+  while(cur < len) {
     let random = Math.floor(Math.random() * len);
     [arr[cur], arr[random]] = [arr[random], arr[cur]];
-    cur--;
+    cur++;
   }
   return arr;
 }
