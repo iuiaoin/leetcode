ec0e94666dc4fffc2a717c7fcf10c046dee9931e
liuxiaolin <478528180@qq.com>
2019-09-04 09:52:44 +0800

    feat: [701] Insert into a Binary Search Tree

---
+++ 701.insert-into-a-binary-search-tree.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=701 lang=javascript
+ *
+ * [701] Insert into a Binary Search Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+/**
+ * 思路：利用bst性质寻找到合适的插入点
+ */
+var insertIntoBST = function(root, val) {
+  if(!root) return null;
+  let node = root;
+  while(node) {
+    if(val < node.val) {
+      if(node.left) {
+        node = node.left;
+      } else {
+        node.left = new TreeNode(val);
+        break;
+      }
+    } else {
+      if(node.right) {
+        node = node.right;
+      } else {
+        node.right = new TreeNode(val);
+        break;
+      }
+    }
+  }
+  return root;
+};
+
