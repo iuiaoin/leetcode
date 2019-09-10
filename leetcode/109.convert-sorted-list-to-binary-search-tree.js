1af9e1f633fd9b5c3077fcbf2ec188e5780f79c9
liuxiaolin <478528180@qq.com>
2019-09-10 16:50:04 +0800

    feat: [109] Convert Sorted List to Binary Search Tree

--- leetcode/109.convert-sorted-list-to-binary-search-tree.js
+++ leetcode/109.convert-sorted-list-to-binary-search-tree.js
@@ -22,28 +22,27 @@
  * @return {TreeNode}
  */
 /**
- * 思路：将list转化维数组，递归的取中间的值作为root，左右两边的数组作为新的数组来构建bst
+ * 思路：遍历list获得长度，以l和r为参数进行中序递归来构建树，得到的即bst
  */
 var sortedListToBST = function(head) {
   if(!head) return null;
-  let arr = [];
-  while(head) {
-    arr.push(head.val);
-    head = head.next;
+  let len = 0,
+      node = head;
+  while(node) {
+    node = node.next;
+    len++;
   }
+  return gen(0, len - 1);
 
-  function gen(arr) {
-    if(!arr.length) return null;
-    let l = 0,
-        r = arr.length - 1,
-        mid = (l + r) >> 1;
-
-    let node = new TreeNode(arr[mid]);
-    node.left = gen(arr.slice(0, mid));
-    node.right = gen(arr.slice(mid + 1));
+  function gen(l, r) {
+    if(l > r) return null;
+    let mid = (l + r) >> 1;
+    let left = gen(l, mid - 1);
+    let node = new TreeNode(head.val);
+    node.left = left;
+    head = head.next;
+    node.right = gen(mid + 1, r);
     return node;
   }
-
-  return gen(arr);
 };
 
