675fe3856723722b947854033b8e1364c89ca1bb
liuxiaolin <478528180@qq.com>
2019-09-18 15:10:27 +0800

    feat: [688] Knight Probability in Chessboard

--- leetcode/688.knight-probability-in-chessboard.js
+++ leetcode/688.knight-probability-in-chessboard.js
@@ -11,28 +11,37 @@
  * @return {number}
  */
 /**
- * 思路：使用dfs，在k move的过程中统计移到board外面和里面的数
+ * 思路：
+ * state: dp[i][j][step]表示在step步之后停留在i, j格子的概率
+ * state transition: dp[i][j][step] = sum{ dp[i + dr][j + dc] / 8};
+ * initialization: 为了减少space使用dp表示dp[][][step - 1]，dp2表示dp[][][step]
  */
 var knightProbability = function(N, K, r, c) {
-  let sum = 0,
-      stay = 0;
-  function dfs(i, j, step, out) {
-    if(i < 0 || i >= N || j < 0 || j >= N) out = true;
-    if(step === 0) {
-      if(!out) stay++; 
-      sum++;
-      return;
+  let dp = new Array(N).fill(null).map(v => new Array(N).fill(0));
+  let dr = [1, 1, 2, 2, -1, -1, -2, -2],
+      dc = [2, -2, 1, -1, 2, -2, 1, -1];
+  dp[r][c] = 1;
+  for(let k = 0; k < K; k++) {
+    let dp2 = new Array(N).fill(null).map(v => new Array(N).fill(0));
+    for(let i = 0; i < N; i++) {
+      for(let j = 0; j < N; j++) {
+        for(let l = 0; l < 8; l++) {
+          let rr = i + dr[l];
+          let cc = j + dc[l];
+          if(rr >= 0 && rr < N && cc >= 0 && cc < N) {
+            dp2[i][j] += dp[rr][cc] / 8;
+          }
+        }
+      }
     }
-    dfs(i - 2, j - 1, step - 1, out);
-    dfs(i - 2, j + 1, step - 1, out);
-    dfs(i + 2, j - 1, step - 1, out);
-    dfs(i + 2, j + 1, step - 1, out);
-    dfs(i - 1, j - 2, step - 1, out);
-    dfs(i - 1, j + 2, step - 1, out);
-    dfs(i + 1, j - 2, step - 1, out);
-    dfs(i + 1, j + 2, step - 1, out);
+    dp = dp2;
   }
-  dfs(r, c, K, false);
-  return stay / sum;
+  let res = 0;
+  for(let i = 0; i < dp.length; i++) {
+    for(let j = 0; j < dp[0].length; j++) {
+      res += dp[i][j];
+    }
+  }
+  return res;
 };
 
