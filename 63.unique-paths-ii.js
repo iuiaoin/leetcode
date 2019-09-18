6d8dc95c0a8483b35fb35d6a9029bb05c32f5c41
liuxiaolin <478528180@qq.com>
2019-09-18 11:14:15 +0800

    feat: [63] Unique Paths II

--- 63.unique-paths-ii.js
+++ 63.unique-paths-ii.js
@@ -12,37 +12,29 @@
  * state: dp[i][j]表示到i, j有多少条路径
  * state transition:grid[i][j] === 1, dp[i][j] = 0 否则 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
  */
-var uniquePathsWithObstacles = function (obstacleGrid) {
-  let grid = obstacleGrid;
-  if (!grid.length) return 0;
-  let m = grid.length,
-    n = grid[0].length,
-    dp = new Array(m).fill(null).map(v => new Array(n).fill(0));
+var uniquePathsWithObstacles = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
 
-  for (let j = 0; j < n; j++) {
-    if(grid[0][j] === 0) {
-      dp[0][j] = 1;
-    } else {
-      for(let k = j; k < n; k++) dp[0][k] = 0;
-      break;
-    }
-  }
-  for (let i = 0; i < m; i++) {
-    if(grid[i][0] === 0) {
-      dp[i][0] = 1;
-    } else {
-      for(let k = i; k < m; k++) dp[i][0] = 0;
-      break;
-    }
-  }
-  for (let i = 1; i < m; i++) {
+  if (grid[0][0] == 1) return 0;
+
+  grid[0][0] = 1;
+
+  for (let i = 1; i < m; i++)
+    grid[i][0] = (grid[i][0] === 0 && grid[i - 1][0] === 1) ? 1 : 0;
+
+  for (let i = 1; i < n; i++)
+    grid[0][i] = (grid[0][i] === 0 && grid[0][i - 1] === 1) ? 1 : 0;
+
+
+  for (let i = 1; i < m; i++)
     for (let j = 1; j < n; j++) {
       if (grid[i][j] === 1) {
-        dp[i][j] = 0;
-      } else {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        grid[i][j] = 0; 
+        continue;
       }
+      grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
     }
-  }
-  return dp[m - 1][n - 1];
+
+  return grid[m - 1][n - 1]
 };
