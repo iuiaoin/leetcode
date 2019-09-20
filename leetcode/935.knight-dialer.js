48482b62489dd6b7e4bb48cbf86321e219cfc961
liuxiaolin <478528180@qq.com>
2019-09-20 11:06:42 +0800

    feat: [935] Knight Dialer

--- leetcode/935.knight-dialer.js
+++ leetcode/935.knight-dialer.js
@@ -9,39 +9,33 @@
  */
 /**
  * 思路：
- * state: dp[i][j][step]代表step步之后，以i, j的数字结尾的number的个数
- * state transition: dp[i][j][step] = sum{dp[i + dr][j + dc][step - 1]}
- * state initialization: dp表示dp[][][step - 1], dp2表示dp[][][step]
+ * state: dp[i][step]step步表示数字i(0~9)结尾的number个数
+ * state transition: dp[i][step] = sum{dp[i对应的knight move数字格子][step - 1]}
+ * state initialization: dp表示dp[][step - 1], dp2表示dp[][step]
  */
 var knightDialer = function (N) {
-  if (N < 1) return 0;
-  const m = 10 ** 9 + 7;
-  let dp = new Array(4).fill(null).map(v => new Array(3).fill(1));
-  dp[3][0] = dp[3][2] = 0;
-  let dr = [1, 1, -1, -1, 2, 2, -2, -2],
-    dc = [2, -2, 2, -2, 1, -1, 1, -1];
-  for (let n = 1; n < N; n++) {
-    let dp2 = new Array(4).fill(null).map(v => new Array(3).fill(0));
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (i === 3 && (j === 0 || j === 2)) continue;
-        for (let k = 0; k < 8; k++) {
-          let r = i + dr[k],
-            c = j + dc[k];
-          if (r >= 0 && r < 4 && c >= 0 && c < 3 || r === 3 && c === 1) {
-            dp2[i][j] += dp[r][c];
-          }
-        }
-        dp2[i][j] %= m;
+  const mod = 10 ** 9 + 7;
+  const map = {
+    1: [6, 8],
+    2: [7, 9],
+    3: [4, 8],
+    4: [0, 3, 9],
+    5: [],
+    6: [0, 1, 7],
+    7: [2, 6],
+    8: [1, 3],
+    9: [2, 4],
+    0: [4, 6],
+  };
+  let dp = new Array(10).fill(1);
+  for (let i = 1; i < N; i++) {
+    let dp2 = new Array(10).fill(0);
+    for (let j = 0; j < 10; j++) {
+      for (let num of map[j]) {
+        dp2[j] += dp[num] % mod;
       }
     }
     dp = dp2;
   }
-  let res = 0;
-  for(let row of dp) {
-    for(let val of row) {
-      res += val;
-    }
-  }
-  return res % m;
+  return dp.reduce((pre, cur) => pre + cur, 0) % mod;
 };
