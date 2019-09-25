073a8ae950b3e2ca20dd3d916d062185e3931e49
liuxiaolin <478528180@qq.com>
2019-09-25 11:31:30 +0800

    feat: [221] Maximal Square

--- leetcode/221.maximal-square.js
+++ leetcode/221.maximal-square.js
@@ -9,61 +9,24 @@
  */
 /**
  * 思路：
- * state: dp[i][j]表示以i, j为右下角的square的大小
- * state transition: 
- * - 若matrix[i][j] = '0', dp[i][j] = 0
- * - 若dp[i - 1][j - 1] = 0, dp[i][j] = 1
- * - 若matrix[i][j]...matrix[i][j - len], matrix[i][j]...matrix[i - len][j] 中先出现的0位置较短长度记为count, dp[i][j] = count * 2
- * - dp[i][j] = (len + 1) ^ 2 ( len为sqrt(dp[i - 1][j - 1]))
- * base case: dp[i][0] = matrix[0][j], dp[0][j] = matrix[0][j]
+ * state: dp[i][j]表示以i, j为右下角的square的边长
+ * state transition: 若matrix[i][j] = '1'，dp[i][j] = min{dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1] } + 1
+ * base case: dp[i][j]初始化为0，从1, 1开始
  */
 var maximalSquare = function (matrix) {
   if (!matrix.length || !matrix[0].length) return 0;
   let m = matrix.length,
     n = matrix[0].length,
-    dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
+    dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
 
-  for (let i = 0; i < m; i++) dp[i][0] = +matrix[i][0];
-  for (let j = 0; j < n; j++) dp[0][j] = +matrix[0][j];
-
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      if (matrix[i][j] === '0') {
-        dp[i][j] = 0;
-      } else if (dp[i - 1][j - 1] === 0) {
-        dp[i][j] = 1;
-      } else {
-        let row = 0,
-          col = 0,
-          flag = false,
-          len = Math.sqrt(dp[i - 1][j - 1]);
-        for (let k = i; k >= i - len; k--) {
-          if (matrix[k][j] === '0') {
-            flag = true;
-            break;
-          }
-          row++;
-        }
-        for (let l = j; l >= j - len; l--) {
-          if (matrix[i][l] === '0') {
-            flag = true;
-            break;
-          }
-          col++;
-        }
-        if (flag) {
-          dp[i][j] = Math.min(row, col) ** 2;
-        } else {
-          dp[i][j] = (len + 1) ** 2;
-        }
+  let sideLen = 0;
+  for(let i = 1; i <= m; i++) {
+    for(let j = 1; j <= n; j++) {
+      if(matrix[i - 1][j - 1] === '1') {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1;
+        sideLen = Math.max(sideLen, dp[i][j]);
       }
     }
   }
-  let res = 0;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      res = Math.max(res, dp[i][j]);
-    }
-  }
-  return res;
+  return sideLen ** 2;
 };
