0ba3991ac2f6c0d98755e2b4fd9ce1500859af27
liuxiaolin <478528180@qq.com>
2019-10-09 14:02:38 +0800

    feat: [787] Cheapest Flights Within K Stops

--- leetcode/787.cheapest-flights-within-k-stops.js
+++ leetcode/787.cheapest-flights-within-k-stops.js
@@ -3,7 +3,6 @@
  *
  * [787] Cheapest Flights Within K Stops
  */
-
 // @lc code=start
 /**
  * @param {number} n
@@ -14,24 +13,105 @@
  * @return {number}
  */
 /**
- * 思路：使用dfs记录从src到dst的所有花费，取最小值
+ * 思路：Dijkstra算法求最短路径
  */
 var findCheapestPrice = function(n, flights, src, dst, K) {
-  if(!flights.length) return -1;
-  let len = flights.length;
-  let res = -1;
-  dfs(src, 0, 0);
-  return res;
-  
-  function dfs(s, cost, k) {
-    if(k <= K + 1 && s === dst) return res = res === -1 ? cost : Math.min(res, cost);
-    if(cost === -1) return;
-    for(let i = 0; i < len; i++) {
-      if(flights[i][0] === s) {
-        dfs(flights[i][1], cost + flights[i][2], k + 1);
-      }
+  let prices = {};
+  for(let f of flights) {
+    if(!prices[f[0]]) prices[f[0]] = {};
+    prices[f[0]][f[1]] = f[2];
+  }
+  let heap = new Heap((a, b) => a[0] - b[0]);
+  heap.add([0, src, K + 1]);
+  while(heap.size()) {
+    let top = heap.poll();
+    let price = top[0];
+    let city = top[1];
+    let stops = top[2];
+    if(city == dst) return price;
+    if(stops > 0) {
+      let adj = prices[city] || {};
+      Object.keys(adj).forEach((a) => {
+        heap.add([price + adj[a], a, stops - 1]);
+      })
     }
   }
+  return -1;
 };
-// @lc code=end
 
+class Heap {
+  constructor(comparator) {
+    this.compare = function(firstEl, secondEl) {
+      return comparator(firstEl, secondEl) <= 0;
+    }
+    this.container = [];
+  }
+  size() {
+    return this.container.length;
+  }
+  add(item) {
+    this.container.push(item);
+    this.heapifyUp();
+  }
+  peek() {
+    if(!this.container.length) return null;
+    return this.container[0];
+  }
+  poll() {
+    if(!this.container.length) return null;
+    if(this.container.length === 1) return this.container.pop();
+    let item = this.container[0];
+    this.container[0] = this.container.pop();
+    this.heapifyDown();
+    return item;
+  }
+  heapifyUp() {
+    let curIndex = this.container.length - 1;
+    while(curIndex > 0 && !this.compare(this.parent(curIndex), this.container[curIndex])) {
+      this.swap(curIndex, this.getParentIndex(curIndex));
+      curIndex = this.getParentIndex(curIndex);  
+    }
+  }
+  heapifyDown() {
+    let curIndex = 0,
+        nextIndex = null;
+    while(this.hasLeftChild(curIndex)) {
+      if(this.hasRightChild(curIndex) && this.compare(this.rightChild(curIndex), this.leftChild(curIndex))) {
+        nextIndex = this.getRightChildIndex(curIndex);
+      } else {
+        nextIndex = this.getLeftChildIndex(curIndex);
+      }
+      if(this.compare(this.container[curIndex], this.container[nextIndex])) break;
+      this.swap(curIndex, nextIndex);
+      curIndex = nextIndex;
+    }
+  }
+  hasLeftChild(parentIndex) {
+    return this.getLeftChildIndex(parentIndex) < this.size();
+  }
+  hasRightChild(parentIndex) {
+    return this.getRightChildIndex(parentIndex) < this.size();
+  }
+  leftChild(parentIndex) {
+    return this.container[this.getLeftChildIndex(parentIndex)];
+  }
+  rightChild(parentIndex) {
+    return this.container[this.getRightChildIndex(parentIndex)];
+  }
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+  parent(childIndex) {
+    return this.container[this.getParentIndex(childIndex)];
+  }
+  swap(index1, index2) {
+    [this.container[index1], this.container[index2]] = [this.container[index2], this.container[index1]];
+  }
+}
+// @lc code=end
