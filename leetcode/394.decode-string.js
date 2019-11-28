2ea79f557bc1239ffd18041bb88453f76d68d960
liuxiaolin <478528180@qq.com>
2019-11-28 09:33:27 +0800

    feat: [394] Decode String

---
+++ leetcode/394.decode-string.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=394 lang=javascript
+ *
+ * [394] Decode String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+/**
+ * 思路：
+ */
+var decodeString = function(s) {
+    
+};
+// @lc code=end
+
