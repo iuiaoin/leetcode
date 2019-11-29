0d91fd436bca635a0c0fd46eaa67ba4831b691f5
liuxiaolin <478528180@qq.com>
2019-11-29 16:26:26 +0800

    feat: [207] Course Schedule

--- leetcode/207.course-schedule.js
+++ leetcode/207.course-schedule.js
@@ -10,35 +10,33 @@
  */
 /**
  * 思路：将课程看为一个n个顶点的有向图，判断图是否有环
+ * 
+ * arr存每个顶点的入度，map存每个顶点对应出度的顶点，queue存入度为0的顶点
  */
-var canFinish = function(numCourses, prerequisites) {
+var canFinish = function (numCourses, prerequisites) {
   let arr = new Array(numCourses).fill(0);
   let map = {};
   let pre = prerequisites;
-  for(let i = 0; i < pre.length; i++) {
+  for (let i = 0; i < pre.length; i++) {
     let [a, b] = pre[i];
     arr[a]++;
     map[b] = (map[b] || []).concat(a);
   }
 
-  while(arr.filter(v => v === 0).length) {
-    let nodes = [];
-    for(let j = 0; j < arr.length; j++) {
-      if(arr[j] === 0) nodes.push(j);
-    }
-    numCourses -= nodes.length;
-    for(let i = 0; i < nodes.length; i++) {
-      let node = nodes[i];
-      arr[node] = Infinity;
-      if(map[node]) {
-        map[node].forEach(val => {
-          arr[val]--;
-        })
-        delete map[node];
-      }
+  let queue = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) queue.push(i);
+  }
+
+  while (queue.length) {
+    let val = queue.shift();
+    numCourses--;
+    if (map[val]) {
+      map[val].forEach(v => {
+        arr[v]--;
+        if (arr[v] === 0) queue.push(v);
+      })
     }
   }
-  
   return numCourses === 0;
 };
-
