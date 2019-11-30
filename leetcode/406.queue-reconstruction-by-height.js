5f5794163f51b5bb694cf34b132e3f94ec25f143
liuxiaolin <478528180@qq.com>
2019-11-30 20:54:56 +0800

    feat: [406] Queue Reconstruction by Height

--- leetcode/406.queue-reconstruction-by-height.js
+++ leetcode/406.queue-reconstruction-by-height.js
@@ -9,6 +9,9 @@
  * @param {number[][]} people
  * @return {number[][]}
  */
+/**
+ * 思路：按照从高到低排序，高度相同则按k值排序，因为前面的是最高的，所以他们的k就是他们所在的位置，之后的people按照k值插入数组中
+ */
 var reconstructQueue = function(people) {
   let res = [];
   people.sort((a, b) => b[0] !== a[0] ? b[0] - a[0] : a[1] - b[1]);
