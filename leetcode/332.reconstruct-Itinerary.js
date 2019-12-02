011b669a6e7972f068ed45ea83462ba55abef26f
liuxiaolin <478528180@qq.com>
2019-12-02 21:56:58 +0800

    feat: [332] Reconstruct Itinerary

--- leetcode/332.reconstruct-Itinerary.js
+++ leetcode/332.reconstruct-Itinerary.js
@@ -10,42 +10,37 @@
  */
 /**
  * 思路：遍历数组，将出发地和目的存入map中，从JFK开始放入结果中
+ * mistake: 应使用栈来存放，因为字符排序小的行程可能没有目的地
+ * 
  */
 var findItinerary = function (tickets) {
   let map = {};
   let res = [];
   if (!tickets.length) return res;
   for (let i = 0; i < tickets.length; i++) {
-    map[tickets[i][0]] = [];
-  }
-  for (let i = 0; i < tickets.length; i++) {
     let [start, end] = tickets[i];
-    if(!map[start].length) {
-      map[start].push(end);
-      continue;
-    }
-    for (let j = 0, len = map[start].length; j < len; j++) {
-      let str = map[start][j];
-      if(map[str]) {
-        if(map[end] && end < str) {
+    if(!map[start]) {
+      map[start] = [ end ];
+    } else {
+      let len = map[start].length;
+      for (let j = 0; j < len; j++) {
+        let str = map[start][j];
+        if(end < str) {
           map[start].splice(j, 0, end);
           break;
-        }
-      } else {
-        if(!map[end] && end < str) {
-          map[start].splice(j, 0, end);
-          break;
+        } else if(j === len - 1) {
+          map[start].push(end);
         }
       }
-      map[start].push(end);
     }
   };
 
-  let key = 'JFK';
-  while (key) {
-    res.push(key);
-    if (!map[key]) break;
-    key = map[key].shift();
+  let stack = ['JFK'];
+  while(stack.length) {
+    while(map[stack[stack.length - 1]] && map[stack[stack.length - 1]].length) {
+      stack.push(map[stack[stack.length - 1]].shift());
+    }
+    res.unshift(stack.pop());
   }
   return res;
 };
