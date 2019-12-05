4ee765420ae749e86e161235f123ae4ecba6bb20
liuxiaolin <478528180@qq.com>
2019-12-05 16:35:05 +0800

    feat: [220] Contains Duplicate III

--- leetcode/220.contains-duplicate-III.js
+++ leetcode/220.contains-duplicate-III.js
@@ -11,17 +11,23 @@
  * @return {boolean}
  */
 /**
- * 思路：滑动窗口，在指针l, l + k之间若存在差值小于等于t的则返回true
+ * 思路：使用size为t+1的桶，桶的个数保持k个，遍历nums若在桶中或前后桶存在且差值小于等于t返回true
  */
 var containsNearbyAlmostDuplicate = function(nums, k, t) {
-  let l = 0;
-  let r = l + k < nums.length ? l + k : nums.length - 1;
-  while(l < r) {
-    for(let j = l + 1; j <= r; j++) {
-      if(Math.abs(nums[l] - nums[j]) <= t) return true;
+  let map = {};
+  if(k < 1 || t < 0) return false;
+  for(let i = 0; i < nums.length; i++) {
+    let bucket = Math.floor(nums[i] / (t + 1));
+    if(map[bucket] !== void 0
+      || map[bucket - 1] !== void 0 && nums[i] - map[bucket - 1] <= t
+      || map[bucket + 1] !== void 0 && map[bucket + 1] - nums[i] <= t) {
+        return true;
+    } 
+    map[bucket] = nums[i];
+    if(i >= k) {
+      let lastBucket = Math.floor(nums[i - k] / (t + 1));
+      delete map[lastBucket];
     }
-    if(r < nums.length - 1) r++;
-    l++;
   }
   return false;
 };
