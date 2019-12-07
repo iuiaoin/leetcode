3e3064832cf2e901cc9fb95088ed74582ca03b85
liuxiaolin <478528180@qq.com>
2019-12-07 15:50:22 +0800

    feat: 思路：以email为key,index为value的map,使用dfs来合并具有相同email的组

--- leetcode/721.accounts-merge.js
+++ leetcode/721.accounts-merge.js
@@ -10,41 +10,43 @@
  * @return {string[][]}
  */
 /**
- * 思路：使用map存accouts的name和email
+ * 思路：以email为key,index为value的map,使用dfs来合并具有相同email的组
  */
 var accountsMerge = function(accounts) {
-  let res = [];
   let map = new Map();
-  for(let i = 0; i < accounts.length; i++) {
-    let name = accounts[i].shift();
-    if(map.has(name)) {
-      let arr = map.get(name);
-      let isDup = false;
-      for(let j = 0; j < arr.length; j++) {
-        let s = new Set([...arr[j], ...accounts[i]]);
-        if(s.size < new Set(accounts[i]).size + arr[j].size) {
-          arr.splice(j, 1);
-          for(let k = 0; k < arr.length; k++) {
-            let set = new Set([...arr[k], ...s]);
-            if(set.size < arr[k].size + s.size) {
-              s = set;
-              arr.splice(k, 1);
-              k--;
-            }
-          }
-          arr.push(s);
-          isDup = true;
-          break;
-        }
+  accounts.forEach((account, index) => {
+    account.slice(1).forEach(email => {
+      if(map.has(email)) {
+        map.get(email).push(index);
+      } else {
+        map.set(email, [index]);
       }
-      if(!isDup) arr.push(new Set(accounts[i]));
-    } else {
-      map.set(name, [new Set(accounts[i])]);
-    }
-  };
-  for(let [key, value] of map) {
-    for(let i = 0; i < value.length; i++) {
-      res.push([key].concat([...value[i]].sort()));
+    });
+  })
+
+  let visit = new Array(accounts.length).fill(false);
+  function dfs(i, emails) {
+    if(visit[i]) return null;
+    visit[i] = true;
+    accounts[i].slice(1).forEach((email) => {
+      if(!emails.has(email)) {
+        emails.add(email);
+        map.get(email).forEach(index => {
+          dfs(index, emails);
+        })
+      }
+    })
+    return emails;
+  }
+  
+  let res = [];
+  for(let i = 0; i < accounts.length; i++) {
+    let name = accounts[i][0];
+    let emails = dfs(i, new Set());
+    if(emails) {
+      let account = [...emails].sort();
+      account.unshift(name);
+      res.push(account);
     }
   }
   return res;
