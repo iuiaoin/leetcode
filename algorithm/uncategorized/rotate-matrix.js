637a218f478243089d6d29842a1bf087825b6a1f
liuxiaolin <478528180@qq.com>
2019-12-10 21:35:20 +0800

    feat: rotate matrix

---
+++ algorithm/uncategorized/rotate-matrix.js
@@ -0,0 +1,9 @@
+/**
+ * 旋转矩阵
+ * 描述：给定一个n*n的矩阵，向右旋转90度
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix) {
+  
+}
