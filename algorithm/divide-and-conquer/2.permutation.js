a8cf2f51be2109433d6cac63da71d60366551c31
liuxiaolin <478528180@qq.com>
2019-12-10 09:16:07 +0800

    feat: permutation

--- algorithm/divide-and-conquer/2.permutation.js
+++ algorithm/divide-and-conquer/2.permutation.js
@@ -23,5 +23,24 @@ function permute(nums) {
 }
 
 // nums中有重复元素
+function permute(nums) {
+  nums.sort((a, b) => a - b);
+  let res = [];
+  let len = nums.length;
+  let visit = new Array(len).fill(false);
+  dfs([]);
+  return res;
 
+  function dfs(cur) {
+    if(cur.length === len) return res.push(cur.slice());
+    for(let i = 0; i < len; i++) {
+      if(visit[i] || i > 0 && nums[i - 1] === nums[i] && !visit[i - 1]) continue;
+      cur.push(nums[i]);
+      visit[i] = true;
+      dfs(cur);
+      cur.pop();
+      visit[i] = false;
+    }
+  }
+}
 
