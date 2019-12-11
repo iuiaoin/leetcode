73820e9c52b621b669d6d107dbef9e50b1fe9088
liuxiaolin <478528180@qq.com>
2019-12-11 21:05:38 +0800

    feat: detect circle

---
+++ algorithm/dfs/detect-circle-in-graphs.js
@@ -0,0 +1,10 @@
+/**
+ * 判圈算法
+ * 描述：给定一个有向图，判断其中是否有环
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+function canFinish(numCourses, prerequisites) {
+  
+}
