d15aec931bc09149a939d8a68af764f4faa1216d
liuxiaolin <478528180@qq.com>
2019-12-14 13:30:39 +0800

    feat: shortest-common-supersequence

--- algorithm/dynamic-programming/7.shortest-common-supersequence.js
+++ algorithm/dynamic-programming/7.shortest-common-supersequence.js
@@ -7,6 +7,7 @@
  */
 /**
  * 先找出str1和str2的LCS，然后把其余部分拼接上去
+ * 在dpArr中存lcs每个char的位置信息，最后根据它来拼接(unnecessary)
  */
 function shortestCommonSupersequence(str1, str2) {
   let len1 = str1.length;
@@ -45,3 +46,39 @@ function shortestCommonSupersequence(str1, str2) {
   res += str1.slice(s1) + str2.slice(s2);
   return res;
 };
+
+
+/**
+ * 使用dp直接返回lcs
+ * 遍历lcs来将str1和str2的字符拼上去
+ */
+function shortestCommonSupersequence(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+  let dp = new Array(len1 + 1).fill(null).map(v => new Array(len2 + 1).fill(''));
+  for(let i = 0; i < len1; i++) {
+    for(let j = 0; j < len2; j++) {
+      if(str1[i] === str2[j]) {
+        dp[i + 1][j + 1] = dp[i][j] + str1[i];
+      } else {
+        dp[i + 1][j + 1] = dp[i][j + 1].length > dp[i + 1][j].length ? dp[i][j + 1] : dp[i + 1][j];
+      }
+    }
+  }
+  let lcs = dp[len1][len2];
+  let i = 0;
+  let j = 0;
+  let res = '';
+  for(let char of lcs) {
+    while(str1[i] !== char) {
+      res += str1[i++];
+    }
+    while(str2[j] !== char) {
+      res += str2[j++];
+    }
+    res += char;
+    i++;
+    j++;
+  }
+  return res + str1.slice(i) + str2.slice(j);
+}
