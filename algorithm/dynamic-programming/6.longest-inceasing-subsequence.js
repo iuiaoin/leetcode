efcb3e530d06b9122dcb59f6deb1272ebce9191f
liuxiaolin <478528180@qq.com>
2019-12-14 09:43:07 +0800

    feat: longest-inceasing-subsequence

--- algorithm/dynamic-programming/6.longest-inceasing-subsequence.js
+++ algorithm/dynamic-programming/6.longest-inceasing-subsequence.js
@@ -4,6 +4,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+/**
+ * state: dp[i]是以nums[i]作为结尾的LIS
+ * state transition: 对于j < i且nums[j] < nums[i], dp[i] = max{dp[j]} + 1
+ * base case: dp[0] = 1
+ */
 function lengthOfLIS (nums) {
-  
+  let len = nums.length;
+  if(!len) return 0;
+  let dp = new Array(len).fill(1);
+  for(let i = 1; i < len; i++) {
+    for(let j = 0; j < i; j++) {
+      if(nums[j] < nums[i]) dp[i] = Math.max(dp[j] + 1, dp[i]);
+    }
+  }
+  return Math.max(...dp);
 }
