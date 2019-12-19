08d18b8d6e14f7b4865f184d0fa0573ca01d2b2f
liuxiaolin <478528180@qq.com>
2019-12-19 14:03:23 +0800

    feat: [151] Reverse Words in a String

---
+++ review/151.reverse-words-in-a-string.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=151 lang=javascript
+ *
+ * [151] Reverse Words in a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s.trim().split(/\s+/).reverse().join(' ');
+};
+// @lc code=end
+
