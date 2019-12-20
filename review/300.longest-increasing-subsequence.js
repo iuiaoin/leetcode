fef112d9b594cdf368fa43def4b4b41104b40e02
liuxiaolin <478528180@qq.com>
2019-12-20 16:23:26 +0800

    feat: [300] Longest Increasing Subsequence

--- review/300.longest-increasing-subsequence.js
+++ review/300.longest-increasing-subsequence.js
@@ -9,18 +9,38 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var lengthOfLIS = function(nums) {
+//   let len = nums.length;
+//   if(!len) return 0;
+//   let dp = new Array(len).fill(1);
+//   for(let i = 1; i < len; i++) {
+//     for(let j = 0; j < i; j++) {
+//       if(nums[j] < nums[i]) {
+//         dp[i] = Math.max(dp[j] + 1, dp[i]);
+//       }
+//     }
+//   }
+//   return Math.max(...dp);
+// };
+
 var lengthOfLIS = function(nums) {
   let len = nums.length;
   if(!len) return 0;
-  let dp = new Array(len).fill(1);
-  for(let i = 1; i < len; i++) {
-    for(let j = 0; j < i; j++) {
-      if(nums[j] < nums[i]) {
-        dp[i] = Math.max(dp[j] + 1, dp[i]);
+  let dp = [];
+  for(let i = 0; i < len; i++) {
+    let l = 0;
+    let r = dp.length - 1;
+    while(l <= r) {
+      let mid = l + r >> 1;
+      if(dp[mid] < nums[i]) {
+        l++;
+      } else {
+        r--;
       }
     }
+    dp[l] = nums[i];
   }
-  return Math.max(...dp);
+  return dp.length;
 };
 // @lc code=end
 
