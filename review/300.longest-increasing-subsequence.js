e7655e2149ce6ed2cfed8dbcd651d38383e3dcc4
liuxiaolin <478528180@qq.com>
2019-12-20 16:07:56 +0800

    feat: [300] Longest Increasing Subsequence

---
+++ review/300.longest-increasing-subsequence.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=300 lang=javascript
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  let len = nums.length;
+  if(!len) return 0;
+  let dp = new Array(len).fill(1);
+  for(let i = 1; i < len; i++) {
+    for(let j = 0; j < i; j++) {
+      if(nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[j] + 1, dp[i]);
+      }
+    }
+  }
+  return Math.max(...dp);
+};
+// @lc code=end
+
