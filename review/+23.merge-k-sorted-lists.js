969885f2f65d3e44ff49c76110c289f4be67f861
liuxiaolin <478528180@qq.com>
2019-12-21 13:07:07 +0800

    feat: [23] Merge k Sorted Lists

--- review/+23.merge-k-sorted-lists.js
+++ review/+23.merge-k-sorted-lists.js
@@ -16,13 +16,13 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-  if(!lists.length) return null;
-  while(lists.length > 1) {
+var mergeKLists = function (lists) {
+  if (!lists.length) return null;
+  while (lists.length > 1) {
     let arr = [];
     let l = 0;
     let r = lists.length - 1;
-    while(l <= r) {
+    while (l <= r) {
       arr.push(mergeTwo(lists[l], lists[r]));
       l++;
       r--;
@@ -32,24 +32,22 @@ var mergeKLists = function(lists) {
   return lists[0];
 
   function mergeTwo(l1, l2) {
-    if(l1 === l2) return l1;
-    let arr = [];
-    while(l1 || l2) {
-      if(!l2 || l1 && l1.val < l2.val) {
-        arr.push(l1.val);
+    if (l1 === l2) return l1;
+    let dummy = new ListNode();
+    let cur = dummy;
+    dummy.next = cur;
+    while (l1 && l2) {
+      if (l1.val < l2.val) {
+        cur.next = l1;
         l1 = l1.next;
       } else {
-        arr.push(l2.val);
+        cur.next = l2;
         l2 = l2.next;
       }
+      cur = cur.next;
     }
-    let list = arr.map(v => new ListNode(v));
-    list.reduce((pre, cur) => {
-      pre.next = cur;
-      return cur;
-    })
-    return list[0];
-  } 
-};
+    if (l1 || l2) cur.next = l1 || l2;
+    return dummy.next;
+  }
+}
 // @lc code=end
-
