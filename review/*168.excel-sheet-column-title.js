5d5ac8deca875d7a737926dcb35b60f832b6d8a2
liuxiaolin <478528180@qq.com>
2019-12-22 17:14:12 +0800

    feat: [168] Excel Sheet Column Title

---
+++ review/*168.excel-sheet-column-title.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=168 lang=javascript
+ *
+ * [168] Excel Sheet Column Title
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function (n) {
+  let map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let res = '';
+  while (n > 0) {
+    n--;
+    let rest = n % 26;
+    n = Math.floor(n / 26);
+    res = map[rest] + res;
+  }
+  return res;
+};
+// @lc code=end
