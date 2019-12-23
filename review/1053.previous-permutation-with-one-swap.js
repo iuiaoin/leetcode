ee93c3ebeac66b8cfdfc584866a13c6dbfe0dfd7
liuxiaolin <478528180@qq.com>
2019-12-23 09:29:57 +0800

    feat: [1053] Previous Permutation With One Swap

---
+++ review/1053.previous-permutation-with-one-swap.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1053 lang=javascript
+ *
+ * [1053] Previous Permutation With One Swap
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var prevPermOpt1 = function(A) {
+    
+};
+// @lc code=end
+
