a3e46210ac9f839460d4586ceb5dd7e09bdb3df0
liuxiaolin <478528180@qq.com>
2019-12-23 10:31:01 +0800

    feat: [1053] Previous Permutation With One Swap

--- review/1053.previous-permutation-with-one-swap.js
+++ review/1053.previous-permutation-with-one-swap.js
@@ -10,7 +10,26 @@
  * @return {number[]}
  */
 var prevPermOpt1 = function(A) {
-    
+  let len = A.length;
+  if(len <= 1) return A;
+  let left = -1;
+  for(let i = len - 1; i > 0; i--) {
+    if(A[i] < A[i - 1]) {
+      left = i - 1;
+      break;
+    }
+  }
+  if(left === -1) return A;
+  let max = -Infinity;
+  let right = -1;
+  for(let i = left + 1; i < len; i++) {
+    if(A[i] < A[left] && A[i] > max) {
+      max = A[i];
+      right = i;
+    }
+  }
+  [A[left], A[right]] = [A[right], A[left]];
+  return A;
 };
 // @lc code=end
 
