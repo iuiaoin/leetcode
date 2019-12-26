8e610c4c4c02d3830b977090a012be2fa792cb08
liuxiaolin <478528180@qq.com>
2019-12-26 11:16:18 +0800

    feat: [142] Linked List Cycle II

--- review/142.linked-list-cycle-ii.js
+++ review/142.linked-list-cycle-ii.js
@@ -18,7 +18,25 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    
+  if(!head) return null;
+  let slow = head;
+  let fast = head;
+  let cur = null;
+  while(fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if(slow === fast) {
+      cur = slow;
+      break;
+    }
+  }
+  if(!cur) return null;
+  let node = head;
+  while(node !== cur) {
+    node = node.next;
+    cur = cur.next;
+  }
+  return node;
 };
 // @lc code=end
 
