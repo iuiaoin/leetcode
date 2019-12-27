e3b1be3e3ec0713e423f723f53e214e3a958272c
liuxiaolin <478528180@qq.com>
2019-12-27 14:40:52 +0800

    feat: [245] Shortest Word Distance III

--- review/+245.shortest-word-distance-iii.js
+++ review/+245.shortest-word-distance-iii.js
@@ -11,21 +11,40 @@
  * @param {string} word2
  * @return {number}
  */
+// var shortestWordDistance = function(words, word1, word2) {
+//   if(!words.length) return Infinity;
+//   let res = Infinity;
+//   let list1 = [];
+//   let list2 = [];
+//   for(let i = 0; i < words.length; i++) {
+//     if(words[i] === word1) list1.push(i);
+//     if(words[i] === word2) list2.push(i);
+//   }
+//   for(let i = 0; i < list1.length; i++) {
+//     for(let j = 0; j < list2.length; j++) {
+//       if(list1[i] === list2[j]) continue;
+//       let dis = Math.abs(list1[i] - list2[j]);
+//       res = Math.min(res, dis);
+//     }
+//   }
+//   return res;
+// };
+
 var shortestWordDistance = function(words, word1, word2) {
-  if(!words.length) return Infinity;
+  let l = Infinity;
+  let r = Infinity;
   let res = Infinity;
-  let list1 = [];
-  let list2 = [];
+  let same = word1 === word2;
   for(let i = 0; i < words.length; i++) {
-    if(words[i] === word1) list1.push(i);
-    if(words[i] === word2) list2.push(i);
-  }
-  for(let i = 0; i < list1.length; i++) {
-    for(let j = 0; j < list2.length; j++) {
-      if(list1[i] === list2[j]) continue;
-      let dis = Math.abs(list1[i] - list2[j]);
-      res = Math.min(res, dis);
+    if(same) r = l;
+    if(words[i] === word1) {
+      l = i;
+    } else if(words[i] === word2) {
+      r = i;
+    } else {
+      continue;
     }
+    res = Math.min(res, Math.abs(l - r));
   }
   return res;
 };
