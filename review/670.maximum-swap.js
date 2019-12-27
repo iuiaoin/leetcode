a2a5042d478cb5933bf76784edea73154d4285f9
liuxiaolin <478528180@qq.com>
2019-12-27 09:22:55 +0800

    feat: [670] Maximum Swap

---
+++ review/670.maximum-swap.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=670 lang=javascript
+ *
+ * [670] Maximum Swap
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function(num) {
+    
+};
+// @lc code=end
+
