cd7852678e0e0bb3350c1b52963927c579c15826
liuxiaolin <478528180@qq.com>
2019-12-30 16:53:48 +0800

    feat: [18] 4Sum

--- review/18.4-sum.js
+++ review/18.4-sum.js
@@ -38,10 +38,28 @@ var fourSum = function(nums, target) {
   let res = [];
   nums.sort((a, b) => a - b);
   for(let i = 0; i < len - 3; i++) {
+    if(i > 0 && nums[i] === nums[i - 1]) continue;
     for(let j = i + 1; j < len - 2; j++) {
-      
+      if(j > i + 1 && nums[j] === nums[j - 1]) continue;
+      let l = j + 1;
+      let r = len - 1;
+      while(l < r) {
+        let sum = nums[i] + nums[j] + nums[l] + nums[r];
+        if(sum === target) {
+          res.push([nums[i], nums[j], nums[l], nums[r]]);
+          while(nums[l] === nums[l + 1]) l++;
+          while(nums[r] === nums[r - 1]) r--;
+          l++;
+          r--;
+        } else if(sum < target) {
+          l++;
+        } else {
+          r--;
+        }
+      }
     }
   }
+  return res;
 };
 // @lc code=end
 
