5e26312d94a63672552c05cc7f227769708f0e86
liuxiaolin <478528180@qq.com>
2020-01-04 17:38:38 +0800

    feat: [198] House Robber

--- review/dynamic-programming/*198.house-robber.js
+++ review/dynamic-programming/*198.house-robber.js
@@ -25,18 +25,33 @@
 //   return dp[len];
 // };
 
+/**
+ * state: dp[i]表示[取nums[i], 不取nums[i]]
+ * state transition: dp[i] = [dp[i - 1][1] + nums[i], max{dp[i - 1][0], dp[i - 1][1]];
+ * base case: dp[0] = [0, 0];
+ */
 var rob = function(nums) {
   let len = nums.length;
   if(!len) return 0;
-  let pre = 0;
-  let cur = nums[0];
-  let res = cur;
-  for(let i = 2; i <= len; i++) {
-    res = Math.max(cur, pre + nums[i - 1]);
-    pre = cur;
-    cur = res;
+  let dp = new Array(len + 1).fill(null).map(v => [0, 0]);
+  for(let i = 1; i <= len; i++) {
+    dp[i] = [dp[i - 1][1] + nums[i - 1], Math.max(dp[i - 1][0], dp[i - 1][1])];
   }
-  return res;
+  return Math.max(...dp[len]);
 };
+
+// var rob = function(nums) {
+//   let len = nums.length;
+//   if(!len) return 0;
+//   let pre = 0;
+//   let cur = nums[0];
+//   let res = cur;
+//   for(let i = 2; i <= len; i++) {
+//     res = Math.max(cur, pre + nums[i - 1]);
+//     pre = cur;
+//     cur = res;
+//   }
+//   return res;
+// };
 // @lc code=end
 
