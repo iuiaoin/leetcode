3d728799a3e570f5b3c4d2714f96630dbce75fb2
liuxiaolin <478528180@qq.com>
2020-01-04 19:08:28 +0800

    feat: [337] House Robber III

--- review/337.house-robber-iii.js
+++ review/337.house-robber-iii.js
@@ -17,7 +17,18 @@
  * @return {number}
  */
 var rob = function(root) {
-    
+  if(!root) return 0;
+  return Math.max(...dfs(root));
+
+  function dfs(node) {
+    if(!node) return [0, 0];
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+    let res = [];
+    res[0] = node.val + left[1] + right[1];
+    res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+    return res;
+  }
 };
 // @lc code=end
 
