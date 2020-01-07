5bdd9ee565a06d3488340a5b4865a27406a8285f
liuxiaolin <478528180@qq.com>
2020-01-07 21:02:45 +0800

    feat: [220] Contains Duplicate III

---
+++ review/+220.contains-duplicate-iii.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=220 lang=javascript
+ *
+ * [220] Contains Duplicate III
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+// var containsNearbyAlmostDuplicate = function(nums, k, t) {
+//   let len = nums.length;
+//   let l = 0;
+//   let r = l + k < len ? l + k : len - 1;
+//   while(l < r) {
+//     for(let i = l + 1; i <= r; i++) {
+//       if(Math.abs(nums[l] - nums[i]) <= t) return true;
+//     }
+//     l++;
+//     if(r < len - 1) r++;
+//   }
+//   return false;
+// };
+
+
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+};
+// @lc code=end
+
